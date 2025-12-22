e17c1bd7df88aa57eaf2fef65b7cada58d2d0bc4
carsons11 <carsons11@nycstudents.net>
2025-12-22 09:26:31 -0500

    Update hero.py

--- hero.py
+++ hero.py
@@ -1,4 +1,14 @@
 class Protagnist():
-    def __init__(self,name)
-        self.name = name
+    def Ryu(self):
+        self.name = "Ryu"
         self.health = 100
+        self.stamina = 100
+        print (f"Here are Ryu's stats: Health:100, Stamina:100")
+    def Ken(self,name):
+        self.name = "Ken"
+
+print ("Choose your characters: Ryu, Ken, M. Bison, and Sagat.")
+x = input("What would you like to choose?")
+Protagnist.Ryu
+
+
