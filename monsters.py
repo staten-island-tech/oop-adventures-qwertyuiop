21383a382763fc8cb40433b07e28fb1dbe3c5dcd
andychen313 <andyc313@nycstudents.net>
2025-12-22 09:26:09 -0500

    Update monsters.py

--- monsters.py
+++ monsters.py
@@ -1,5 +1,11 @@
-class Monster:
-    def __init__(self, name):
-        self.name = name
+class Antagonist:
+    def Bison(self, name):
+        self.name = "M. Bison"
         self.health = 100
+        self.stamina = 100
+print("Choose a character: Ryu, Ken, Sagat, M. Bison")
+name = input("What would you like to choose?: ")
+characters = Antagonist()
+if name == "M. Bison":
+    print(characters.Bison(name))
         
