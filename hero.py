efe63f1d09a4e5f398c6bfaa169b5deb446a6cbe
carsons11 <carsons11@nycstudents.net>
2026-01-05 09:18:28 -0500

    Update hero.py

--- hero.py
+++ hero.py
@@ -1,15 +1,11 @@
-class Protagnist:
-    def __init__(self,name):
-        self.name = name
-        self.health = 1000
-        self.stamina = 1000
-        print (f"Here are {name}'s stats: Health:{self.health}, Stamina:{self.stamina}")
-    def Zelda(self):
-        self.name = "Zelda"
-        self.health = 1000
-        self.stamina = 1000
-name = input("What is your character's name?:")
-self = Protagnist(f"{name}")
+class monsters:
+    def Bokoblins(self):
+        self.name = "Bokoblins"
+        self.health = 13
+    def BlueBokoblins(self):
+        self.name = "Blue Bokoblins"
+
+
 
 
 
