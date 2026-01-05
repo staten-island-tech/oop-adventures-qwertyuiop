05a8e8a60f7fb38760e61435592a68ba03e3e206
andychen313 <andyc313@nycstudents.net>
2026-01-05 09:02:01 -0500

    commit

--- monsters.py
+++ monsters.py
@@ -1,21 +1,5 @@
-""" class Antagonist:
-    def Bison(self):
-        self.name = "M. Bison"
-        self.health = 1000
-        self.stamina = 1000
-    def Sagat(self):
-        self.name = "Sagat"
-        self.health = 1000
-        self.stamina = 1000
-
-def user_interaction():
-    character = Antagonist(interaction)
-    interaction = input("Who do you want to choose?: ")
-    if interaction == "Sagat":
-        print(f"You just picked Sagat")
-        Sagat.Antagonist(character) """
-
-characters = 
+class Monster:
+    
 
 
 
