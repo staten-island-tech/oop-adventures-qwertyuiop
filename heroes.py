7f2b8e41d38fc155b54ea0125b208573d597dc4b
andychen313 <andyc313@nycstudents.net>
2026-01-06 09:26:38 -0500

    Update heroes.py

--- heroes.py
+++ heroes.py
@@ -1,8 +1,56 @@
-Champions = {
-    "Link", "Mipha", "Daruk", "Urbosa", "Revali"
+Champions = [
+
+{
+    "name": "YoYo",
+    "faction": "Champion",
+    "health": 120,
+    "description": "YoYo is a legendary warrior who was chosen to be Lumy's knight in shining armor. However, the champion has just awoken from his deep slumber."
+},
+
+{
+    "name": "KoKo",
+    "faction": "Champion",
+    "health": 115,
+    "description": "KoKo is the elegant and gentle warrior of Leafy Town, the idolized leader."
+},
+
+{
+    "name": "DoDo",
+    "faction": "Champion",
+    "health": 135,
+    "description": "DoDo is the charismatic and light-hearted champion from Jumbo City. From the fiery depths of Death Mountain, Daruk protects his allies with his flaming personality and defense."
+},
+
+{
+    "name": "Uro",
+    "faction": "Champion",
+    "health": 110,
+    "description": "Uro is the shining and charismatic warrior of Goo Mania, protecting her people from the desert storms to monsters from the deep evils."
+},
+
+{
+    "name": "Riko",
+    "faction": "Champion",
+    "health": 120,
+    "description": "Riko is the soaring champion of the skies, protecting his friends from above from gails to flying arrows."
+},
+
+{
+    "name": "Lumy",
+    "faction": "Royal",
+    "health": 35,
+    "description": "Lumy is the princess of Goo Mania, protected by her loving knights and warriors who would lay their life down to serve her."
+
 }
 
+]
 
+""" class Enter: """
+
+def enter():
+        print("Welcome to Doria, the world of multiple opportunities. There exists evils and different social classes. You are YoYo, here is your description: ")
+        print(Champions[0])
+enter()
 
 
 
