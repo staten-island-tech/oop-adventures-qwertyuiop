b74360c2b57be165ea89a792dfdc78b22f250a42
carsons11 <carsons11@nycstudents.net>
2026-01-06 09:26:41 -0500

    commiy

---
+++ Enemies.py
@@ -0,0 +1,46 @@
+Monsters = [
+
+{
+    "name": "Boko",
+    "faction": "Royal",
+    "health": 45
+},
+
+{
+    "name":"Blue Boko",
+    "faction": "Prestige",
+    "health": 65
+},
+
+{
+    "name": "Black Boko",
+    "faction": "God",
+    "health": 85
+},
+
+{
+    "name": "Silver Boko",
+    "faction": "Champion",
+    "health": 110
+},
+
+{
+    "name": "Golden Boko",
+    "faction": "Champion",
+    "health": 150
+},
+
+{
+    "name": "Zook",
+    "faction": "Royal",
+    "health": 35
+}
+]
+
+class 
+
+
+
+
+
+
