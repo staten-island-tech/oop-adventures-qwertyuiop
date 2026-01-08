191aa2de3aa5b0c6902844e88dfe998853053cae
carsons11 <carsons11@nycstudents.net>
2026-01-08 07:47:12 -0500

    Update Enemies.py

--- Enemies.py
+++ Enemies.py
@@ -3,52 +3,63 @@ Monsters = [
 {
     "name": "Boko",
     "faction": "Royal",
-    "health": 45
+    "health": 45,
+    "atk": 5
 },
 
 {
     "name":"Blue Boko",
     "faction": "Prestige",
-    "health": 65
+    "health": 65,
+    "atk": 15
 },
 
 {
     "name": "Black Boko",
     "faction": "God",
-    "health": 85
+    "health": 85,
+    "atk": 20
 },
 
 {
     "name": "Silver Boko",
     "faction": "Champion",
-    "health": 110
+    "health": 110,
+    "atk": 25
 },
 
 {
     "name": "Golden Boko",
     "faction": "Champion",
-    "health": 150
+    "health": 150,
+    "atk": 35
 },
 
 {
     "name": "Zook",
     "faction": "Royal",
-    "health": 35
+    "health": 35,
+    "atk":3
 }
 ]
 
 class Monsters():
+    def __init__(self,hp,atk):
+        self.hp = hp
+        self.atk = atk
     def Boko_fight():
-        atk = 
+        atk = 5
+        print ("Boko executed a ")
     def Blue_Boko_fight():
-
+        atk = 15
     def Black_Boko_fight():
-
+        atk = 20
     def Silver_Boko_fight():
-
+        atk = 25
     def Golden_Boko_fight():
-
-
+        atk:35
+    def Zook_fight():
+        atk = 3
 
 
 
