4dbc2136401d39a12b3632bcbecdc62a06ac9959
carsons11 <carsons11@nycstudents.net>
2026-01-08 09:17:56 -0500

    Update Enemies.py

--- Enemies.py
+++ Enemies.py
@@ -53,18 +53,22 @@ class Monsters():
     def __init__(self,hp,atk):
         self.hp = hp
         self.atk = atk
-    def Boko_fight():
-        atk = 5
+    def Boko_fight(self):
+        self.hp = 45
+        self.atk = 5
         print ("Boko executed a leaf hurricane that barely winded you. 5 health was lost.")
-    def Blue_Boko_fight():
-        atk = 15
-    def Black_Boko_fight():
-        atk = 20
-    def Silver_Boko_fight():
-        atk = 25
-    def Golden_Boko_fight():
+    def Blue_Boko_fight(self):
+        self.hp = 65
+        self.atk = 15
+    def Black_Boko_fight(self):
+        self.hp = 85
+        self.atk = 20
+    def Silver_Boko_fight(self):
+        self.hp = 110
+        self.atk = 25
+    def Golden_Boko_fight(self):
         atk:35
-    def Zook_fight():
+    def Zook_fight(self):
         atk = 3
 
  
