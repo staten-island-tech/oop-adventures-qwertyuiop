786ed23b66c32e77594dd0697f378a3e2f46d28f
andychen313 <andyc313@nycstudents.net>
2026-01-08 09:26:16 -0500

    Create heroes.py

---
+++ heroes.py
@@ -0,0 +1,34 @@
+class Heroes:
+    def __init__(self):
+        self.name="YoYo"
+        self.health = 120
+        self.faction= "Champion"
+        self.description = "YoYo is a legendary warrior who was chosen to be Lumy's knight in shining armor. However, the champion has just awoken from his deep slumber."
+    
+    attacks = [
+
+{
+    "name": "Spinkick",
+            "atk": 15
+},
+
+{
+    "name": "sweep Kick",
+            "atk": 8
+},
+
+        {
+            "name": "Left Hook",
+            "atk": 25
+        },
+
+        {
+            "name": "Uppercut Combo",
+            "atk": 30
+        }
+
+    ]
+
+
+
+
