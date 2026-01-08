43e41e70ff8d650f5d693a2c8985cdbc3f95a811
carsons11 <carsons11@nycstudents.net>
2026-01-08 09:00:12 -0500

    Update Enemies.py

--- Enemies.py
+++ Enemies.py
@@ -1,88 +1,54 @@
-<<<<<<< HEAD
-# Monsters = [
-# {
-#     "name": "Boko",
-#     "faction": "Royal",
-#     "health": 45
-# },
-# {
-#     "name":"Blue Boko",
-#     "faction": "Prestige",
-#     "health": 65
-# },
-# {
-#     "name": "Black Boko",
-#     "faction": "God",
-#     "health": 85
-# },
-# {
-#     "name": "Silver Boko",
-#     "faction": "Champion",
-#     "health": 110
-# },
-# {
-#     "name": "Golden Boko",
-#     "faction": "Champion",
-#     "health": 150
-# },
-# {
-#     "name": "Zook",
-#     "faction": "Royal",
-#     "health": 35
-# }
-# ]
-class Boko():
-    def __init__(self):
-        self.name = "Boko"
-        self.health = 45
-        self.faction = "Royal"
-    def takedamage(self, damage):
-        damage = 
+
+# class Boko():
+#     def __init__(self):
+#         self.name = "Boko"
+#         self.health = 45
+#         self.faction = "Royal"
+#     def takedamage(self, damage):
+#         damage = 
         
-
-
-
-=======
 Monsters = [
-
 {
     "name": "Boko",
-    "faction": "Royal",
+    "faction": "Mini-Boss",
+    "city patron": "Leafy Town",
     "health": 45,
     "atk": 5
 },
 
 {
     "name":"Blue Boko",
-    "faction": "Prestige",
+    "faction": "Boss",
+    "city patron": "Jumbo City",
     "health": 65,
     "atk": 15
 },
 
 {
     "name": "Black Boko",
-    "faction": "God",
+    "faction": "Boss",
+    "city patron": "Death Mountain",
     "health": 85,
     "atk": 20
 },
 
 {
     "name": "Silver Boko",
-    "faction": "Champion",
+    "faction": "Boss",
     "health": 110,
     "atk": 25
 },
 
 {
     "name": "Golden Boko",
-    "faction": "Champion",
+    "faction": "Boss",
     "health": 150,
     "atk": 35
 },
 
 {
     "name": "Zook",
-    "faction": "Royal",
+    "faction": "Mini-Boss",
     "health": 35,
     "atk":3
 }
@@ -93,19 +59,27 @@ class Monsters():
         self.hp = hp
         self.atk = atk
     def Boko_fight():
+        hp = 45
         atk = 5
-        print ("Boko executed a ")
+        print ("Boko executed Leaf Hurricane and dealt 5 damage.")
     def Blue_Boko_fight():
+        hp = 65
         atk = 15
+        print ("Blue Boko shot an oversized hammer at you which bounces off. 15 damage was dealt.")
     def Black_Boko_fight():
+        hp = 85
         atk = 20
+        print ("Black Boko activated his necro ability and skeletons swarm at you for 20 damage.")
     def Silver_Boko_fight():
+        hp = 110
         atk = 25
+        print ("")
     def Golden_Boko_fight():
+
         atk:35
     def Zook_fight():
         atk = 3
->>>>>>> 191aa2de3aa5b0c6902844e88dfe998853053cae
+
 
 
 
