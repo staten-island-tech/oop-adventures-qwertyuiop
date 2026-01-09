d1039871f7eab9cba7159c20dfeccc6c7aeccbc6
andychen313 <andyc313@nycstudents.net>
2026-01-09 08:56:05 -0500

    Update heroes.py

--- heroes.py
+++ heroes.py
@@ -5,33 +5,34 @@ class Heroes:
         self.faction= "Champion"
         self.description = "YoYo is a legendary warrior who was chosen to be Lumy's knight in shining armor. However, the champion has just awoken from his deep slumber."
     
-    attacks = [
+attacks = [
 
-        {
-            "name": "Spinkick",
-            "atk": 15
-        },
+{
+    "name": "Spinkick",
+    "atk": 15
+},
 
-        {
-            "name": "Sweep kick",
-            "damage": 8
-        },
+{
+    "name": "Sweep kick",
+    "damage": 8
+},
 
-        {
-            "name": "Left hook",
-            "damage": 25
-        },
+{
+    "name": "Left hook",
+    "damage": 25
+},
 
-        {
-            "name": "Uppercut combo",
-            "damage": 30
-        }
-    ]
+{
+    "name": "Uppercut combo",
+    "damage": 30
+}
 
-    def show_attacks(self):
-        print("What attack do you want to execute?")
-        for i, item in enumerate(self.attacks):
-            print(i, ":", item['name'])
+]
+
+def show_attacks():
+    print("What attack do you want to execute?")
+    for i, item in enumerate(attacks):
+        print(i, ":", item['name'])
     choice = int(input("Enter your choice: "))
     if choice == 0:
         print("You used spinkick! You dealt 15 damage!")
@@ -43,8 +44,7 @@ class Heroes:
         print("You used uppercut combo! You dealt 30 damage!")
     else:
         print("Invalid choice!")
-
-
+show_attacks()
 
 
 
