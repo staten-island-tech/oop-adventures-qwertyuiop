4ba328a9ad84984b7432cd1ed11c3170274cd8f3
andychen313 <andyc313@nycstudents.net>
2026-01-14 09:26:35 -0500

    commit

--- game.py
+++ game.py
@@ -2,7 +2,7 @@ from place import Map
 from heroes import Attacks, Monsters
 
 Map.location
-monsters = Monsters(25,25)
+monsters = Monsters(45, 5)
 Attacks.show_attacks()
 monsters.Boko_fight()
 
--- heroes.py
+++ heroes.py
@@ -48,50 +48,60 @@ class Attacks:
             print("Invalid choice!")
     show_attacks()
 
-
 class Monsters():
     def __init__(self,hp,atk):
             self.hp = hp
             self.atk = atk
+    
+    def Boko(self):
+          self.hp = 125
+          self.atk = 15
+    
     def Boko_fight(self):
-            self.hp = 45
-            self.atk = 5
             Heroes.health - Monsters.atk
             print ("Boko executed a leaf hurricane that barely winded you. 5 health was lost.")
-            print(Heroes.health)
+    
+    def Blue_Boko(self):
+          self.hp = 65
+          self.atk = 55
+    
     def Blue_Boko_fight(self):
-            self.hp = 65
-            self.atk = 15
             Heroes.health - Monsters.atk
-            print(Heroes.health)
             print ("Blue Boko threw jumbo sized machinery at you dealing a total of 15 damage.")
+    
+    def Black_Boko(self):
+          self.hp = 85
+          self.atk = 45
+    
     def Black_Boko_fight(self):
-            self.hp = 85
-            self.atk = 20
             Heroes.health - Monsters.atk
             print ("Black Boko raised the dead and skeletons swarm for 20 damage.")
-            print(Heroes.health)
+    
+    def Silver_Boko(self):
+          self.hp = 110
+          self.atk = 25
+    
     def Silver_Boko_fight(self):
-            self.hp = 110
-            self.atk = 25
-            Heroes.health - Monsters.atk
-            print ("Silver Boko jumps and rains a gumdrop barrage. You lost 25 health.")
-            print(Heroes.health)
+        Heroes.health - Monsters.atk
+        print ("Silver Boko jumps and rains a gumdrop barrage. You lost 25 health.")
+    
+    def Golden_Boko(self):
+         self.hp = 150
+         self.atk = 35
+    
     def Golden_Boko_fight(self):
-            self.hp = 150
-            self.atk = 35
             Heroes.health - Monsters.atk
             print ("Moving as fast as wind, the infamous Black Boko hit a Wuxi Fingerhold that deals 35 damage.")
-            print(Heroes.health)
+    
+    def Zook(self):
+        self.hp = 115
+        self.atk = 25
+    
     def Zook_fight(self):
-            self.hp = 35
-            self.atk = 3
             Heroes.health - Monsters.atk
             print ("Zook tried to deafen your ears with Drake's Passion Fruit song but only dealt 3 damage.")
-            print(Heroes.health)
 
-
-
+class 
 
 
 
